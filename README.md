013e5d0d48b0142b08032d32e0d60647b7152a21
Ignacio Medina Castillo <raising_spirit@hotmail.com>
2016-09-27 20:05:05 +0200

    Upload base working code
    Initial Submit

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # PromiseChain
 Light weight javascript library to handle promises control structures with ease.
+
+
+
+This library is about structure control mechanisms to handle promises, like if/else, or while.
+
+It work with a inside scope to save all the results of the multiple promises you can launch.
+
+
+
+## How to use
+
+The following example run multiple promises waiting until the end of the previous.
+
+
+```js
+PromiseChain = require('PromiseChain.js');
+
+var doSomething = function(scope){
+	return new Promise(function(resolve, reject) { setTimeout(function(){
+		console.log("I have done something");
+	 	resolve(true);
+	
+	} , 1000);});
+}
+
+
+var askPermision = function(scope){
+	return new Promise(function(resolve, reject) { setTimeout(function(){
+
+	 	resolve(true);
+	
+	} , 1000);});
+}
+
+var incrementCounter = function(scope){
+	return new Promise(function(resolve, reject) { 	setTimeout(function(){
+	 	  	scope.counter++;
+	 	  	resolve(scope.counter);
+	 	 } , 1000); 	
+	 });
+}
+
+var scope = {contador :0};
+
+new PromiseChain(sc)
+	.continue(doSomething,"fisrtThingDone")
+	.continue(doSomething,"secondThingDone")
+	.continue(askPermision,"permision")
+	.ifElse(function(sc){return sc.permision === true;},function(sc){
+		return 'if';
+	},function(sc){
+		return 'else';
+	},'ifelseResult')
+	.if(function(sc){return sc.contador == 0},function(sc){
+		return 'if';
+	},'ifResult')
+	.while(function(sc){return sc.contador <= 5;}, function(sc){
+		console.log(sc.ifelseResult + " - " + sc.ifResult);
+		console.log(sc.contador);
+		return new Promise(function(resolve, reject) { 
+	 	  		setTimeout(function(){
+	 	  			sc.contador++;
+	 	  			resolve(sc.contador);
+	 	  		} , 1000); 	
+	 	  });
+		
+	},'whileResults')
+	
+	.continue(function(sc){
+		return new Promise(function(resolve, reject) { 
+				console.log(sc.whileResults);
+				console.log(sc);
+	 	  		setTimeout(function(){
+	 	  			resolve(sc.contador*3);
+	 	  		} , 1000); 	
+	 	  });
+	})
+	.end();
+
+```
---
+++ node/PromiseChain.js
@@ -0,0 +1,125 @@
+module.exports = function (scope,functionPropietary){
+		var me = this;
+		me.functionPropietary = functionPropietary || false;
+		me.scope = scope || {} ;	
+
+		me.currentLink = new Promise(function(resolve, reject) { 
+			resolve(me.scope);
+		});
+
+		me.bindToPropietary = function(fun){
+			if (me.functionPropietary){
+				return fun.bind(me.functionPropietary);
+			}
+			return fun;
+		};
+
+		me.execute = function(fun,resultName,nextLinkResolve){
+			Promise.resolve(fun(me.scope)).then(function(result){
+				me.scope[resultName] = result;
+				nextLinkResolve(result);
+			}).catch(console.log.bind(console));
+		};
+
+		me.continue = function(fun,resultName){
+			resultName = resultName || '[[lastResult]]';
+			fun = me.bindToPropietary(fun);
+			
+			var nextLinkResolve = null;
+			var nextLink = new Promise(function(resolve, reject) { 	nextLinkResolve = resolve;	});
+
+			me.currentLink.then(function (){
+				me.execute(fun,resultName,nextLinkResolve); 
+			}).catch(console.log.bind(console)); 
+
+			me.currentLink = nextLink;
+			return me;
+		};
+
+		me.ifElse = function(condition,iffun,elsefun,resultName){
+			resultName = resultName || '[[lastResult]]';
+			iffun = me.bindToPropietary(iffun);
+			elsefun = me.bindToPropietary(elsefun);
+
+			var nextLinkResolve = null;
+			var nextLink = new Promise(function(resolve, reject) { 	nextLinkResolve = resolve; });
+
+			me.currentLink.then(function (){
+				if (condition(me.scope) === true){
+					me.execute(iffun,resultName,nextLinkResolve); 
+				}else{
+					me.execute(elsefun,resultName,nextLinkResolve); 
+				}
+			}).catch(console.log.bind(console)); 
+
+			me.currentLink = nextLink;
+			return me;
+
+		};
+
+		me.if = function(condition,fun,resultName){
+			return me.ifElse(condition,fun,function(){},resultName);
+		};
+		me.while = function(condition,fun,resultName){
+			resultName = resultName || '[[lastResult]]';
+			fun = me.bindToPropietary(fun);
+
+			var nextLinkResolve = null;
+			var nextLink = new Promise(function(resolve, reject) { 	nextLinkResolve = resolve;	});
+
+			me.currentLink.then(function (){
+				me.scope[resultName] = [];
+				me.continueWhile(condition,fun,resultName,nextLinkResolve);	
+			}).catch(console.log.bind(console)); 
+
+			me.currentLink = nextLink;
+			return me;
+		};
+
+		me.continueWhile = function(condition,fun,resultName,nextLinkResolve){
+			if (condition(me.scope) === true){
+		 		Promise.resolve(fun(me.scope)).then(function(result){
+					me.scope[resultName].push(result);
+					me.continueWhile(condition,fun,resultName,nextLinkResolve);	 
+				}).catch(console.log.bind(console));
+			}else{
+				nextLinkResolve(me.scope[resultName]);
+			}	
+		 };
+
+
+		me.continueAll = function(diffTerms,fun,resultName){
+			resultName = resultName || '[[lastResult]]';
+			fun = me.bindToPropietary(fun);
+			
+			var nextLinkResolve = null;
+			var nextLink = new Promise(function(resolve, reject) { nextLinkResolve = resolve;	});
+			var tempArray = [];
+	
+			if (!Array.isArray(diffTerms)){for (var term in diffTerms){tempArray.push(term);}diffTerms = tempArray;}
+			
+
+            me.currentLink.then(function (){
+            	var promises = [];
+	            for (var i = 0; i < diffTerms.length; i++) {
+	            	promises.push(Promise.resolve(fun(me.scope,diffTerms[i])));
+	            }
+	            Promise.all(promises).then(function(result){
+	            	var hashedResult = {};
+	            	for (var i = 0; i < diffTerms.length; i++) {
+	            		hashedResult[diffTerms[i]] = result[i];
+	            	}
+					me.scope[resultName] = hashedResult;
+					nextLinkResolve(hashedResult);
+				}).catch(console.log.bind(console)); 
+			}).catch(console.log.bind(console)); 
+
+			me.currentLink = nextLink;
+			return me;
+		};
+
+		me.end = function(){
+
+			return me.currentLink;
+		};
+	};
